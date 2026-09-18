a2023b9dfa6543a763785b52659139edf28c082c
antonioleeps <42138581+antonioleeps@users.noreply.github.com>
2026-09-18 17:06:54 +0200

    Corr. SS_Model for NoMemory

--- Matching_ChoiceSymmetricQLearningNoMemory/Matching_SS_MLE_ChoiceSymmetricQLearningNoMemory_Model.m
+++ Matching_ChoiceSymmetricQLearningNoMemory/Matching_SS_MLE_ChoiceSymmetricQLearningNoMemory_Model.m
@@ -4,41 +4,43 @@ ChoiceLeft = SessionData.Custom.TrialData.ChoiceLeft(1:nTrials);
 Rewarded = SessionData.Custom.TrialData.Rewarded(1:nTrials);
 
 % Parametric estimation
-LowerBound = [0.10, 2, 0.05, -0.5];
-UpperBound = [0.45, 10, 0.25, 0.5];
-
-% Free parameters
-% 20241220 tested with simulation that works well as initial parameters
-LearningRate = 0.25; % alpha
-InverseTemperature = 8; % beta
-ForgettingRate = 0.15; % gamma
-Bias = 0;
-
-InitialParameters = [LearningRate, InverseTemperature, ForgettingRate, Bias];
+LowerBound = [0.05, -10, 0.05, -2];
+UpperBound = [0.65,  20, 0.45,  2];
 
 CalculateMLE = @(Parameters) ChoiceSymmetricQLearningNoMemory(Parameters, nTrials, ChoiceLeft, Rewarded);
 
-try
-    [EstimatedParameters, MinNegLogDataLikelihood, ~, ~, ~, Grad, Hessian] =...
-        fmincon(CalculateMLE, InitialParameters, [], [], [], [], LowerBound, UpperBound);
-catch
-    disp('Error: fail to run model');
-    EstimatedParameters = [];
-    MinNegLogDataLikelihood = nan;
-end
-
+Model = struct();
 Model.LowerBound = LowerBound;
 Model.UpperBound = UpperBound;
-Model.InitialParameters = InitialParameters;
+Model.MinNegLogDataLikelihood = Inf;
 
-Model.EstimatedParameters = EstimatedParameters;
-Model.MinNegLogDataLikelihood = MinNegLogDataLikelihood;
-Model.Grad = Grad;
-Model.Hessian = Hessian;
-try
-    Model.ParameterStandardError = sqrt(diag(inv(Hessian)))';
-catch
-    Model.ParameterStandardError = nan(size(EstimatedParameters));
+for iInitialCond = 1:20
+    InitialParameters...
+        = LowerBound + rand * (UpperBound - LowerBound);
+
+    try
+        [EstimatedParameters, MinNegLogDataLikelihood, ~, ~, ~, Grad, Hessian] =...
+            fmincon(CalculateMLE, InitialParameters, [], [], [], [], LowerBound, UpperBound);
+    catch
+        disp('Error: fail to run model');
+        EstimatedParameters = [];
+        MinNegLogDataLikelihood = nan;
+    end
+    
+    if Model.MinNegLogDataLikelihood > MinNegLogDataLikelihood
+        Model.LowerBound = LowerBound;
+        Model.UpperBound = UpperBound;
+        Model.InitialParameters = InitialParameters;
+        
+        Model.EstimatedParameters = EstimatedParameters;
+        Model.MinNegLogDataLikelihood = MinNegLogDataLikelihood;
+        Model.Grad = Grad;
+        Model.Hessian = Hessian;
+        try
+            Model.ParameterStandardError = sqrt(diag(inv(Hessian)))';
+        catch
+            Model.ParameterStandardError = nan(size(EstimatedParameters));
+        end
+    end
 end
-
 end % end function
